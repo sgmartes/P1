5df5b9564c720f96cdbf35ad80a99741cd96ab89
Sebastian <jaimepeters2@gmail.com>
2023-03-03 09:11:56 -0600

    la función ahora devuelve la cant de primos

--- Post-lab/Primos.c
+++ Post-lab/Primos.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<math.h>
 #define TAM_MAX 1000
-#define CANT_PRIMOS 30
-void primos_criba(int n, int primos[]){
-    int indice=2; //Empezamos a buscar primos desde el 2, ya que ni el 0 ni el 1 son primos.
-    int lista[TAM_MAX]={[0 ... 999]=1};//Se inicializan todos los elementos a 1
+#define CANT_PRIMOS 100
+int primos_criba(int n, int primos[]){
+    int indice=2; 
+    int lista[TAM_MAX]={[0 ... 999]=1};
     
-    for (indice;indice<=n;++indice){//Se pasan a cero los elementos cuyo índice no sea primo
+    for (indice;indice<=n;++indice){
         if (lista[indice]){
             for (int i=2;i*indice<=n;++i){
                 lista[i*indice]=0;
@@ -15,23 +15,26 @@ void primos_criba(int n, int primos[]){
     }
 
     int indice_secundario=0;
-    for (indice=2;indice<=n;++indice){//Los índices primos se guardan en el arreglo "primos"
+    for (indice=2;indice<=n;++indice){
         if (lista[indice]){
             primos[indice_secundario]=indice;
             ++indice_secundario;
         }
     }
+    
 
     for (int i=0;i<indice_secundario;i++){
         printf("%d ",primos[i]);
     }
     printf("\n\n"); 
+    return indice_secundario;
     //Se imprime el arreglo
     //Este último for es solo para probar que el programa funciona.
     //No incluírlo en la HdT <3
-
+    
 }
 int main(){
     int primos[CANT_PRIMOS];
-    primos_criba(CANT_PRIMOS,primos);
+    int cantidad=primos_criba(CANT_PRIMOS,primos);
+    printf("Primos encontrados: %d\n",cantidad);
 }
